6d7db11d3e7172807dd0c19fe07aabdcd06a8286
Kiran-Vellanki <kiranvellanki10@gmail.com>
2023-04-22 10:21:07 +0530

    builder modified

--- Creational/Builder.java
+++ Creational/Builder.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 interface Item {
     String name();
 
@@ -41,6 +43,134 @@ abstract class NonVegPizza extends Pizza {
 
 }
 
-public class Builder {
+class SmallPizza extends VegPizza {
+    String name;
+    String size;
+    float price;
 
+    public SmallPizza(String name, String size, float price) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    @Override
+    public String name() {
+        return this.name;
+    }
+
+    @Override
+    public String size() {
+        return this.size;
+    }
+
+    @Override
+    public float price() {
+        return this.price;
+    }
+}
+
+abstract class Coke extends ColdDrink {
+    @Override
+    public abstract float price();
+
+}
+
+abstract class Pepsi extends ColdDrink {
+    @Override
+    public abstract float price();
+}
+
+class SmallCoke extends Coke {
+    public SmallCoke() {
+    }
+
+    String name;
+    String size;
+    float price;
+
+    public SmallCoke(String name, String size, float price) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    @Override
+    public String name() {
+        return "coke";
+    }
+
+    @Override
+    public String size() {
+        return "small coke";
+    }
+
+    @Override
+    public float price() {
+        return 40;
+    }
+}
+
+class MediumPizza extends Pizza {
+    public MediumPizza() {
+    }
+
+    String name;
+    String size;
+    float price;
+
+    public MediumPizza(String name, String size, float price) {
+        this.name = name;
+        this.size = size;
+        this.price = price;
+    }
+
+    @Override
+    public String name() {
+        return "Medium pizza";
+    }
+
+    @Override
+    public String size() {
+        return "Medium";
+    }
+
+    @Override
+    public float price() {
+        return 60f;
+    }
+
+}
+
+class OrderItems {
+    ArrayList<Item> item = new ArrayList<>();
+
+    public void addItem(Item i) {
+        item.add(i);
+    }
+
+    public float getCost() {
+        float cost = 0;
+        for (Item item2 : item) {
+            cost += item2.price();
+        }
+        return cost;
+    }
+
+    public void getName() {
+        for (Item item2 : item) {
+            System.out.println(item2.name());
+        }
+    }
+}
+
+public class Builder {
+    public static void main(String[] args) {
+        OrderItems items = new OrderItems();
+        Item item = new MediumPizza();
+        Item item2 = new SmallCoke();
+        items.addItem(item);
+        items.addItem(item2);
+        System.out.println(items.getCost());
+    }
 }
